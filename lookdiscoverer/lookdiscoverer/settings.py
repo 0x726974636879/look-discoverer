bb43dcc08957c7c623a22f31f9f23dcaa778216f
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2021-07-14 00:24:10 +0200

    modify(settings.py): change REST_FRAMEWORK.PAGE_SIZE to 10

--- lookdiscoverer/lookdiscoverer/settings.py
+++ lookdiscoverer/lookdiscoverer/settings.py
@@ -133,7 +133,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 50
+    'PAGE_SIZE': 10
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
