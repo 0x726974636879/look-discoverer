91a5508dbd976b1f2767d2d8647a33bd8b576d3c
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2021-07-14 00:20:04 +0200

    modify(Look.js): add icon img

--- lookdiscoverer-front/src/components/Look.js
+++ lookdiscoverer-front/src/components/Look.js
@@ -1,11 +1,20 @@
+import { CardMedia } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
+
 
 
 const Look = ({ look, handleOpenModal }) => {
     return (
         <tr>
             <th>{look.look_id}</th>
-            <td><a href={`https://images.heuritech.com/${look.image_md5}`} title={look.look_name}>{look.look_name}</a></td>
+            <th>
+                <CardMedia
+                    style={{width: "50px", height: "50px"}}
+                    image={`https://images.heuritech.com/${look.image_md5}`}
+                    title={look.look_name}
+                />
+            </th>
+            <td>{look.look_name}</td>
             <td>{look.country}</td>
             <td>{look.hype_count}</td>
             <td>{look.hashtags}</td>
